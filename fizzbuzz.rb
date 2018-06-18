32c5fbf9a89521e5579e36964d615b97763c4723
Jamie Brown <jmbrowncow@gmail.com>
2018-06-18 21:42:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
   if x % 3 == 0 && x % 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif x % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif x % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else nil
   end
 end
